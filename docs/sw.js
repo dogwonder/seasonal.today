1d35fefca0847b0aeeee180556a2fca245a41879
Rich Holman <dogwonder@gmail.com>
2019-11-12 14:56:51 +0000

    move README

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "stCache-1.130.0";
+const CACHE = "stCache-1.131.0";
 
 // This is the service worker with the Cache-first network
 const precacheFiles = [
@@ -37,7 +37,7 @@ self.addEventListener("activate", function(event) {
         })
       );
     }).then(function() {
-      console.log('[ServiceWorker] Claiming clients for version - 1.130.0');
+      console.log('[ServiceWorker] Claiming clients for version - 1.131.0');
       return self.clients.claim();
     })  
   );
