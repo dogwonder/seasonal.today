c6bb6d1e193f315aa6ac5ad0d7b5f15c6a1d21ad
dogwonder <dogwonder@gmail.com>
2022-07-08 10:04:59 +0100

    Revert "update readme"
    This reverts commit e7ec6d138dcec4495e103d404c5f81cdaa24a836.
    
    # Conflicts:
    #	README.md
    #	assets/scripts/babel/scripts.js
    #	assets/scripts/scripts.js
    #	assets/styles/main.scss
    #	assets/styles/partials/_blocks.scss
    #	docs/blocks.html
    #	docs/css/main.css
    #	docs/index.html
    #	docs/offline.html
    #	docs/scripts/app.js
    #	docs/sw.js
    #	package.json
    #	src/styles/partials/_animate.scss

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,76 @@
+(function () {
+
+    //remove no-js class
+    document.documentElement.className = document.documentElement.className.replace("no-js","js");
+
+    //🍪 notice
+    function cookieNoticeSeen() {
+
+        var getCookie = function (name) {
+            var value = "; " + document.cookie;
+            var parts = value.split("; " + name + "=");
+            if (parts.length == 2) return parts.pop().split(";").shift();
+        };
+
+        // Cookie vars
+        var cookieNotice = document.getElementById('cookieNotice');
+        var cookieButton = document.getElementById('cookieButton');
+
+        // Set a cookie
+        cookieButton.addEventListener('click', function (event) {
+            Cookies.set('jw_cookie_notice', 'closed', { expires: 365, path: '' });
+            cookieNotice.classList.remove('open');
+            document.body.classList.remove('has-cookie');
+        }, false);
+
+        //Remove notice if cookie already set
+        if (Cookies.get('jw_cookie_notice') == 'closed') {
+            cookieNotice.classList.remove('open');
+            document.body.classList.remove('has-cookie');
+        }
+
+    }
+
+    function equalizer(elem) {
+        const mq = window.matchMedia( "(min-width: 1025px)" );
+        var blocks = document.querySelectorAll(elem);
+        if(!blocks) return;
+        
+        if (mq.matches) {
+            var tallest = 0; 
+            // Loop over matching divs
+            blocks.forEach(function (item) {
+                var height = item.offsetHeight;
+                tallest = (height>tallest ? height : tallest); 
+                // console.log(tallest);
+                item.style.height = tallest + "px";
+            });
+        }
+
+    }
+
+    // Handler when the DOM is fully loaded
+    document.addEventListener("DOMContentLoaded", function(){
+
+        //Cookie notice
+        cookieNoticeSeen();
+
+        //Equal height columns
+        equalizer('[data-col]');
+
+        //Load the cookie policy functionality
+        var cookie = new WJCookies();
+        cookie.init({
+            onClick: true,
+            onSubmit: false
+        });
+        // cookie.init();
+        
+    });
+
+    // window.addEventListener("resize", function(){
+    //     equalizer('[data-col]');
+    // });
+
+})();
+
